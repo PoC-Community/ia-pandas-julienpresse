4f189c1154d4d13f173f604287e72821d1c4891e
ju <julien.presse@epitec.eu>
2023-11-21 19:49:43 +0100

    exo 7 page 2

--- page2.py
+++ page2.py
@@ -24,3 +24,14 @@ print(missing_count)
 numerical_data = data.select_dtypes(include='number')
 column_names = numerical_data.columns
 print(column_names)
+
+columns_to_remove = ['Critic_Score', 'Critic_Count', 'User_Score', 'User_Count', 'Developer', 'Rating']
+data = data.drop(columns=columns_to_remove)
+numerical_data = data.select_dtypes(include='number')
+column_names = numerical_data.columns
+
+correlation_matrix = numerical_data.corr()
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title('Correlation Heatmap of Numerical Values')
+plt.show()
